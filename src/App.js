6be69374c614edb488b958e6f149ff40937fdabf
grzegorzkowalski <grzegorzkowalski@yahoo.com>
2021-10-15 23:00:11 +0200

    Routing

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import {BrowserRouter, Switch, Route} from "react-router-dom";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Register from "./components/Register";
+
+import './App.css';
+
+function App() {
+    return (
+        <BrowserRouter>
+           <Switch>
+               <Route exact path="/" component={Home}/>
+               <Route exact path="/login" component={Login}/>
+               <Route exact path="/register" component={Register}/>
+           </Switch>
+        </BrowserRouter>
+    );
+}
+
+export default App;
---
+++ src/components/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            Strona główna
+        </div>
+    );
+};
+
+export default Home;
